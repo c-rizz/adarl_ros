53d03902a55e10ec8183aaf5ba223d16630c3c3a
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-09-12 15:20:01 +0200

    starting to add xmj env to adapter

--- adarl_ros/src/adarl_ros/adapters/XbotMjAdapter.py
+++ adarl_ros/src/adarl_ros/adapters/XbotMjAdapter.py
@@ -15,6 +15,8 @@ import numpy as np
 
 import time
 import torch as th
+import rospy
+import os
 
 class XbotMjAdapter(BaseSimulationAdapter, RosXbotAdapter
     ):
@@ -24,8 +26,12 @@ class XbotMjAdapter(BaseSimulationAdapter, RosXbotAdapter
     """
 
     def __init__(self,
+        model_fpath: str,
         model_name : str,
-        stepLength_sec : float,
+        xbot2_config_path: str = None,
+        headless: bool = False,
+        init_steps: int = 0,
+        timeout_ms: int = 1000,
         forced_ros_master_uri : Union[str, None] = None,
         maxObsDelay = float("+inf"),
         blocking_observation = False,
@@ -44,6 +50,20 @@ class XbotMjAdapter(BaseSimulationAdapter, RosXbotAdapter
         """Initialize the Simulator.
 
         """
+        self._model_fpath=model_fpath
+        self._xbot2_config_path=xbot2_config_path
+        self._headless=headless
+        self._init_steps=init_steps
+        self._timeout_ms=timeout_ms
+        self._closed=False
+
+        self._xmj_sim_env=None
+        self._loader_helper = LoadingUtils(model_name)
+
+        self._init_simulation() # after this, all data from sim is available
+        # for reading
+
+        stepLength_sec=self._xmj_env.physics_dt#
         super().__init__(model_name=model_name,
                         stepLength_sec=stepLength_sec,
                         forced_ros_master_uri=forced_ros_master_uri,
@@ -60,7 +80,29 @@ class XbotMjAdapter(BaseSimulationAdapter, RosXbotAdapter
                         jpos_cmd_max_acc=jpos_cmd_max_acc,
                         jpos_cmd_max_acc_default=jpos_cmd_max_acc_default,
                         enable_filters=enable_filters)
-        
+    
+    def _init_simulation(self):
+        self._xmj_env = XBotMjSimEnv(
+            model_fname=self._model_fpath,
+            xbot2_config_path=self._xbot2_config_path,
+            headless=self._headless,
+            manual_stepping=True,
+            init_steps=self._init_steps,
+            timeout=self._timeout_ms # [ms]
+        )
+        self._abs_sim_timer=0
+        self._xmj_env_jnt_names=self._xmj_env.jnt_names()
+        self._xmk_evn_n_dofs=self._xmj_env.n_jnts()
+
+    def __del__(self):
+        self._close()
+
+    def _close(self):
+        if not self._closed:
+            if self._xmj_env.is_running():
+                self._xmj_env.close()
+            self._closed=True
+
     def setJointsStateDirect(self, jointStates : Dict[Tuple[str,str],JointState]):
         raise NotImplementedError()
 
@@ -83,59 +125,62 @@ class XbotMjAdapter(BaseSimulationAdapter, RosXbotAdapter
         raise NotImplementedError()
     
     def getEnvTimeFromStartup(self) -> float:
-        raise NotImplementedError()
+        self._abs_sim_timer+=self.getEnvTimeFromReset()
+        return self._abs_sim_timer
     
     def getEnvTimeFromReset(self) -> float:
-        raise NotImplementedError()
+        return self._xmj_env.physics_dt*self._xmj_env.step_counter
     
     def set_monitored_joints(self, jointsToObserve : List[Tuple[str,str]]):
-        raise NotImplementedError()
+        super().set_monitored_joints(jointsToObserve=jointsToObserve)
     
     def set_monitored_links(self, linksToObserve : List[Tuple[str,str]]):
-        raise NotImplementedError()
+        super().set_monitored_links(linksToObserve=linksToObserve)
     
     def set_monitored_cameras(self, camerasToRender : List[str] = []):
-        raise NotImplementedError()
+        super().set_monitored_cameras(camerasToRender=camerasToRender)
     
     def run(self, duration_sec : float):
-        raise NotImplementedError()
+        self._apply_controls()
+        while self.getEnvTimeFromReset()<duration_sec:
+            self.step()
     
     def startup(self):
-        raise NotImplementedError()
+        super().startup()
+        rospy.loginfo("ROS time is "+str(rospy.get_time())+" pid = "+str(os.getpid()))
+        self.resetWorld()
+
+    def step(self):
+        self._xmj_env.step()
+
+    def xmj_env(self):
+        return self._xmj_env
     
     def resetWorld(self):
-        raise NotImplementedError()
-    
-    def getJointsState(self, requestedJoints : List[Tuple[str,str]]) -> Dict[Tuple[str,str],JointState]:
-        raise NotImplementedError()
-    
-    def getLinksState(self, requestedLinks : List[Tuple[str,str]]) -> Dict[Tuple[str,str],LinkState]:
-        raise NotImplementedError()
-    
-    def getRenderings(self, requestedCameras : List[str]) -> Dict[str, Tuple[np.ndarray, float]]:
-        raise NotImplementedError()
-    
-    def setJointsStateDirect(self, jointStates : Dict[Tuple[str,str],JointState]):
-        raise NotImplementedError()
-    
-    def setLinksStateDirect(self, linksStates : Dict[Tuple[str,str],LinkState]):
-        raise NotImplementedError()
-    
+        super(RosXbotAdapter, self).resetWorld()
+        self.clear_commands()
+
+        self._last_jdi_time = -1
+        self._last_joint_device_info = None
+        req_mask = 255
+        mask = self._setup_joint_control(control_mask = req_mask)
+        if mask != req_mask:
+            raise RuntimeError(f"Failed to set control mask, wanted {req_mask}, got {mask}")
+        switched_on = self._switch_control(True, timeout_s = 300.0)
+        if not switched_on:
+            raise RuntimeError(f"Failed to switch on control.")
+        
+        self._xmj_env.reset()
+
     def _setup_joint_control(self, control_mask : int, timeout_s = 300.0) -> int:
         mask = -1
         def check_done():
             # This will be run at the end of the async run. Either if it times out
             # or if it completes (successfully or not)
             nonlocal mask
-            # ggLog.info(f"callback: mask = {mask}")
             if mask != control_mask:
                 raise RuntimeError(f"Failed to switch control to {mask}.")
         self.run_async(duration_sec=timeout_s, on_finish_callback=check_done)
-        # self._gazeboAdapter.unpauseSimulation()
-        mask = super()._setup_joint_control(control_mask, timeout_s=timeout_s)
-        # ggLog.info(f"_setup_joint_control returned {control_mask}")
-        # ggLog.info(f"control_mask = {control_mask}")
-        # self._gazeboAdapter.pauseSimulation()
         self.stop_run_async()
         self.wait_run_async() # check_done will always be run before this
         # check_done()
@@ -148,16 +193,73 @@ class XbotMjAdapter(BaseSimulationAdapter, RosXbotAdapter
             # This will be run at the end of the async run. Either if it times out
             # or if it completes (successfully or not)
             nonlocal switched_on
-            # ggLog.info(f"callback: switched_on = {switched_on}")
             if switched_on != switch_on:
                 raise RuntimeError(f"Failed to switch control to {switch_on}.")
         self.run_async(duration_sec=timeout_s, on_finish_callback=check_done)
-        # self._gazeboAdapter.unpauseSimulation()
         switched_on = super()._switch_control(switch_on, timeout_s=timeout_s)
-        # ggLog.info(f"switch_control returned {switched_on}")
-        # ggLog.info(f"switched_on = {switched_on}")
-        # self._gazeboAdapter.pauseSimulation()
         self.stop_run_async()
         self.wait_run_async() # check_done will always be run before this
         # check_done()
         return switched_on
+
+    def getJointsState(self, requestedJoints : List[Tuple[str,str]]) -> Dict[Tuple[str,str],JointState]:
+        
+        jnts_q=self._xmj_env.jnts_q
+        jnts_v=self._xmj_env.jnts_v
+        jnts_a=self._xmj_env.jnts_a
+        jnts_eff=self._xmj_env.jnts_eff
+
+        ret = {}
+        for rj in requestedJoints:
+            jnt_idx=-1
+            try:
+                jnt_idx=self._xmj_env_jnt_names.index(rj)    
+            except:
+                ggLog.info(f"Joint {rj} not found in available joint. Cannot read state.")
+                continue        
+            jointState = JointState(list(jnts_q[jnt_idx]),
+                                    list(jnts_v[jnt_idx]),
+                                    list(jnts_eff[jnt_idx]))
+            ret[rj] = jointState
+
+        return ret
+
+    def getLinksState(self, requestedLinks : List[Tuple[str,str]]) -> Dict[Tuple[str,str],LinkState]:
+        self._xmj_env.p
+        self._xmj_env.q
+        self._xmj_env.twist
+        self._xmj_env.jnts_q
+
+        ret = {}
+        for rl in requestedLinks:
+            if not (("base_link" in rl) or ("root_link" in rl)):
+                ggLog.info(f"getLinksState currently supports reading base link state only!")
+            else:
+                linkState = LinkState(position_xyz = (self._xmj_env.p[0], self._xmj_env.p[1], self._xmj_env.p[2]),
+                    orientation_xyzw = (self._xmj_env.q[1], self._xmj_env.q[2], self._xmj_env.q[3], self._xmj_env.q[0]),
+                    pos_velocity_xyz = (self._xmj_env.twist[0], self._xmj_env.twist[1]. self._xmj_env.twist[2]),
+                    ang_velocity_xyz = (self._xmj_env.twist[3], self._xmj_env.twist[4], self._xmj_env.twist[5]))
+                ret[rl] = linkState
+        return ret
+    
+    def getRenderings(self, requestedCameras : List[str]) -> Dict[str, Tuple[np.ndarray, float]]:
+        raise NotImplementedError()
+    
+    def setJointsStateDirect(self, jointStates : Dict[Tuple[str,str],JointState]):
+        """Set the state for a set of joints
+
+        Parameters
+        ----------
+        jointStates : Dict[Tuple[str,str],JointState]
+            Keys are in the format (model_name, joint_name), the value is the joint state to enforce
+        """
+        controlled_joints = {jn:js for jn,js in jointStates.items() if jn in self.get_controlled_joints()}
+        uncontrolled_joints = {jn:js for jn,js in jointStates.items() if jn not in self.get_controlled_joints()}
+
+        super().moveToJointPoseSync(jointPositions={jn:js.position.item() for jn,js in controlled_joints.items()},
+                                    velocity_scaling=1.0,
+                                    acceleration_scaling=1.0,
+                                    joint_position_tolerance=0.05)
+
+    def setLinksStateDirect(self, linksStates : Dict[Tuple[str,str],LinkState]):
+        raise NotImplementedError()
